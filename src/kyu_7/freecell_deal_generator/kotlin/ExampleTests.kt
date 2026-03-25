7cb8d101b01cb4aaefb5a96d198fae13cf841c31
vnook_baboolee <now-kirill@ya.ru>
2026-03-25 22:11:31 +0300

    FreeCell Deal Generator

---
+++ src/kyu_7/freecell_deal_generator/kotlin/ExampleTests.kt
@@ -0,0 +1,31 @@
+//package kyu_7.freecell_deal_generator.kotlin
+//
+//import org.jetbrains.spek.api.Spek
+//import kotlin.test.assertEquals
+//
+//class ExampleTests: Spek({
+//    test("Deal #1") {
+//        val deal1 = listOf(
+//            "JD", "2D", "9H", "JC", "5D", "7H", "7C", "5H",
+//            "KD", "KC", "9S", "5S", "AD", "QC", "KH", "3H",
+//            "2S", "KS", "9D", "QD", "JS", "AS", "AH", "3C",
+//            "4C", "5C", "TS", "QH", "4H", "AC", "4D", "7S",
+//            "3S", "TD", "4S", "TH", "8H", "2C", "JH", "7D",
+//            "6D", "8S", "8D", "QS", "6C", "3D", "8C", "TC",
+//            "6S", "9C", "2H", "6H"
+//        )
+//        assertEquals(deal1, deal(1))
+//    }
+//    test("Deal #11982") {
+//        val deal11982 = listOf(
+//            "AH", "AS", "4H", "AC", "2D", "6S", "TS", "JS",
+//            "3D", "3H", "QS", "QC", "8S", "7H", "AD", "KS",
+//            "KD", "6H", "5S", "4D", "9H", "JH", "9S", "3C",
+//            "JC", "5D", "5C", "8C", "9D", "TD", "KH", "7C",
+//            "6C", "2C", "TH", "QH", "6D", "TC", "4S", "7S",
+//            "JD", "7D", "8H", "9C", "2H", "QD", "4C", "5H",
+//            "KC", "8D", "2S", "3S"
+//        )
+//        assertEquals(deal11982, deal(11982))
+//    }
+//})
