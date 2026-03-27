6d5201340731c6a95ffb83aca62552cffa8ecdfa
vnook_baboolee <now-kirill@ya.ru>
2026-03-27 08:45:50 +0300

    Tricky Kotlin #3: define a function

---
+++ src/kyu_6/tricky_kotlin_3_define_a_function/kotlin/concatString.kt
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/59b3d479623654e11000006f/train/kotlin
+
+package kyu_6.tricky_kotlin_3_define_a_function.kotlin
+
+val concatString: (String, String) -> String = String::plus
