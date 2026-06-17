72815a4bb1588e201d6e7c2f9ba657874b35e3e1
vnook_dedoolee <now-kirill@ya.ru>
2026-06-17 14:53:47 +0300

    Zig-Zag Camel

---
+++ src/kyu_6/zig_zag_camel/kotlin/TestZigZagCamel.kt
@@ -0,0 +1,15 @@
+package kyu_6/zig_zag_camel/kotlin
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("Tests")
+class TestZigZagCamel {
+  @Test
+  @DisplayName("test")
+  fun test() {
+    // assertEquals(expected, actual, delta)
+    assertEquals(Math.hypot(10.0, 5.0), zigZagCamel(10.0, 5.0), 1e-9);
+  }
+}
