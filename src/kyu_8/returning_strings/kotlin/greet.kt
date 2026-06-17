d6efe08a9c0962e018761d376306d7c4a95cdf98
vnook_dedoolee <now-kirill@ya.ru>
2026-06-17 14:33:30 +0300

    Returning Strings

---
+++ src/kyu_8/returning_strings/kotlin/greet.kt
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/train/kotlin
+
+package kyu_8.returning_strings.kotlin
+
+fun greet(name: String): String {
+    return "Hello, $name how are you doing today?"
+}
