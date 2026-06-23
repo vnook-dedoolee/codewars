90471175e5b40b1d8ae7a3639e238cb7f2392849
vnook_dedoolee <now-kirill@ya.ru>
2026-06-23 10:14:01 +0300

    Bagels

---
+++ src/kyu_4/bagels/kotlin/bagel.kt
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/54bd6b4c956834c9870001a1/train/kotlin
+
+val bagel: Bagel
+	get() = Bagel()
+object org{object junit{object Assert{fun assertEquals(a: Any?, b: Any?){}}}}
